801801466bc7248a7fe3783be2af0fd2b6449535
Nikolay Avramenko <avr.nic@ya.ru>
2022-12-02 23:44:43 +0300

    Implemented the functionality of adding and receiving users

--- cmd/main.go
+++ cmd/main.go
@@ -10,17 +10,26 @@ func main() {
 	fmt.Println("Hello world")
 
 	u := service.User{Login: "log", Email: "em", Mobile: "8-800", Name: "name", Password: "pass"}
+	u2 := service.User{Login: "Sixzer", Email: "6zer", Mobile: "8-999-999-99-99", Name: "Alex", Password: "qwe"}
+
+	existingUser := service.User{Login: "log", Mobile: "8-800", Name: "name", Password: "pass"}
 
 	js, err := json.Marshal(u)
 	if err != nil {
 		fmt.Println(err)
 	} else {
-		service.CreateUser(js)
+		service.CreateUserFromJSON(js)
 	}
 
+	service.CreateUser(u2)
+
+	service.CreateUser(existingUser)
+
+	service.PrintUsers()
+
 	u1, err2 := service.GetUser("log")
-	if err2 != nil {
-		fmt.Println(u1.Email)
+	if err2 == nil {
+		fmt.Println(u1)
 	} else {
 		fmt.Println(err2)
 	}
--- pkg/service/users.go
+++ pkg/service/users.go
@@ -5,70 +5,57 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
 )
 
+// string for password hashing
 const salt = "dfvjfewfewdvndojpbe"
 
-//type passwordEnconder interface {
-//	generatePasswordHash(password string) string
-//}
-
+// an alias that can be used anywhere in place of a  type name
 type key string
 
 type User struct {
 	Login    key    `json:"login"`
-	Email    string `json:"email"`
-	Mobile   string `json:"mobile" , omitempty`
+	Email    string `json:"email" ,omitempty`
+	Mobile   string `json:"mobile"`
 	Name     string `json:"name"`
 	Password string `json:"password"`
 	//BirthDay date   `json:"birth_day", omitempty`
 }
 
+// function for password encryption
 func generatePasswordHash(password string) string {
 	hash := sha1.New()
 	hash.Write([]byte(password))
 	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
 }
 
+// an alias that can be used anywhere in place of a type name
 type usersMap map[key]User
 
-var users = usersMap{}
+// structure for storing users
+var users = make(usersMap)
 
-// Можно добавить проверку по email
-func (u usersMap) checkUserID(ID key) bool {
-	if _, ok := u[ID]; ok {
-		fmt.Println("User ", ID, " exist")
+// the function of verifying the existence of a user with this key
+func (u usersMap) checkUserID(keyValue key) bool {
+	if _, ok := u[keyValue]; ok {
+		fmt.Println("User ", keyValue, " exist")
 		return true
+	}
+	return false
+}
+
+// GetUser the function returns the user by key
+func GetUser(keyValue key) (User, error) {
+	if exist := users.checkUserID(keyValue); exist {
+		return users[keyValue], nil
 	} else {
-		fmt.Println("User not found")
-		return false
+		return User{}, errors.New("the User does not exist")
 	}
 }
 
-func (u *User) WriteJSON(w io.Writer) error {
-	js, err := json.Marshal(u)
-	if err != nil {
-		return err
-	}
-	_, err = w.Write(js)
-	return err
-}
-
-//func ReadJSON(w io.Writer) error {
-//	var u User
-//	err := json.Unmarshal(w, u)
-//	if err != nil {
-//		return err
-//	}
-//	_, err = w.Write(js)
-//	return err
-//}
-
-func CreateUser(b []byte) bool {
-	u := User{}
-	err := json.Unmarshal(b, &u)
-	if err == nil && !users.checkUserID(u.Login) {
+// CreateUser the function adds a new user
+func CreateUser(u User) bool {
+	if !users.checkUserID(u.Login) {
 		u.Password = generatePasswordHash(u.Password)
 		users[u.Login] = u
 		return true
@@ -76,23 +63,20 @@ func CreateUser(b []byte) bool {
 	return false
 }
 
-func GetUser(login key) (User, error) {
-	if exist := users.checkUserID(login); exist {
-		return users[login], nil
-	} else {
-		return User{}, errors.New("the User does not exist")
+// CreateUserFromJSON CreateUser the function adds a new user from JSON
+func CreateUserFromJSON(b []byte) bool {
+	u := User{}
+	err := json.Unmarshal(b, &u)
+	if err == nil {
+		return CreateUser(u)
 	}
+	return false
 }
 
-//func NewUsers() usersMap {
-//	return make(usersMap)
-//}
-
-//func main() {
-//
-//	//us := make(usersMap)
-//
-//	//	us.CreateUser()
-//	//	us.CreateUser(11)
-//	//	us.CreateUser(10)
-//}
+// PrintUsers the function displays information about users
+func PrintUsers() {
+	fmt.Println("Print users:")
+	for _, value := range users {
+		fmt.Println(value)
+	}
+}
