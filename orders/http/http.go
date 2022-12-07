08f415707637bb3c78b20b0dd0c7eaf2db927a26
Лысов Глеб <lysovg9@gmail.com>
2022-12-07 04:13:54 +0300

    Added: grouping

--- orders/http/http.go
+++ orders/http/http.go
@@ -1,35 +1,34 @@
 package http
 
 import (
-	"context"
 	"fmt"
 	"net/http"
 	"orders/table"
+	"reflect"
 	"regexp"
 	"strings"
 )
 
 type HttpApp struct {
-	router Router
-	states map[string]interface{}
+	Router Router
+	State map[string]interface{}
+}
+
+type HttpGroup struct {
+	path string
+	app *HttpApp
 }
 
 func NewApp() *HttpApp {
 	return &HttpApp{
-		router: Router{},
-		states: make(map[string]interface{}),
+		Router: Router{},
+		State: make(map[string]interface{}),
 	}
 }
 
 func (h *HttpApp) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// setting knows state
-	for key, state := range h.states {
-		ctx := context.WithValue(r.Context(), key, state)
-		r = r.WithContext(ctx)
-	}
-
-	h.router.RunPreMiddleware(w, r)
-	h.router.ServeHTTP(w, r)
+	h.Router.RunPreMiddleware(w, r)
+	h.Router.ServeHTTP(w, r, &h.State)
 }
 
 func (a *HttpApp) Run(port string) error {
@@ -40,7 +39,7 @@ func (a *HttpApp) Run(port string) error {
 func (a *HttpApp) MakeInfoTable(port string) string {
 	var table table.TableBuilder
 
-	for _, route := range a.router.routes {
+	for _, route := range a.Router.routes {
 		table.AppendRoute(route.Method, route.OrigPath)
 	}
 
@@ -72,39 +71,70 @@ func parsePattern(pattern string) *regexp.Regexp {
 	return regexp.MustCompile(buffer.String())
 }
 
-func (a *HttpApp) Manage(key string, state interface{}) {
-	a.states[key] = state
+func (a *HttpApp) Manage(state interface{}) {
+	key := reflect.TypeOf(state).String()
+	a.State[key] = state
 }
 
-func (a *HttpApp) SetHandler(method string, strPattern string, handler RouterHandler) {
+func (a *HttpApp) SetHandler(method string, strPattern string, handler RouteHandler) {
 	pattern := parsePattern(strPattern)
 
-	newroutes := append(a.router.routes, Route{
+	newroutes := append(a.Router.routes, Route{
 		OrigPath: strPattern,
 		Method:   method,
 		Pattern:  pattern,
 		Handler:  handler,
 	})
 
-	a.router.routes = newroutes
+	a.Router.routes = newroutes
 }
 
-func (a *HttpApp) Get(pattern string, handler RouterHandler) {
+func (a *HttpApp) Get(pattern string, handler RouteHandler) {
 	a.SetHandler(http.MethodGet, pattern, handler)
 }
-func (a *HttpApp) Post(pattern string, handler RouterHandler) {
+func (a *HttpApp) Post(pattern string, handler RouteHandler) {
 	a.SetHandler(http.MethodPost, pattern, handler)
 }
-func (a *HttpApp) Delete(pattern string, handler RouterHandler) {
+func (a *HttpApp) Delete(pattern string, handler RouteHandler) {
 	a.SetHandler(http.MethodDelete, pattern, handler)
 }
-func (a *HttpApp) Patch(pattern string, handler RouterHandler) {
+func (a *HttpApp) Patch(pattern string, handler RouteHandler) {
 	a.SetHandler(http.MethodPatch, pattern, handler)
 }
-func (a *HttpApp) Put(pattern string, handler RouterHandler) {
+func (a *HttpApp) Put(pattern string, handler RouteHandler) {
 	a.SetHandler(http.MethodPut, pattern, handler)
 }
 
-func (a *HttpApp) Use(handler RouterHandler) {
-	a.router.middlewares = append(a.router.middlewares, handler)
+func (a *HttpApp) Use(handler MiddlwareRouterHandler) {
+	a.Router.middlewares = append(a.Router.middlewares, handler)
+}
+
+func (a *HttpApp) Group(path string) *HttpGroup {
+	return &HttpGroup {
+		path: path,
+		app: a,
+	}
+}
+
+func (g *HttpGroup) Group(path string) *HttpGroup {
+	return &HttpGroup {
+		path: g.path + path,
+		app: g.app,
+	}
+}
+
+func (g *HttpGroup) Get(pattern string, handler RouteHandler) {
+	g.app.SetHandler(http.MethodGet, g.path + pattern, handler)
+}
+func (g *HttpGroup) Post(pattern string, handler RouteHandler) {
+	g.app.SetHandler(http.MethodPost, g.path + pattern, handler)
+}
+func (g *HttpGroup) Delete(pattern string, handler RouteHandler) {
+	g.app.SetHandler(http.MethodDelete, g.path + pattern, handler)
+}
+func (g *HttpGroup) Patch(pattern string, handler RouteHandler) {
+	g.app.SetHandler(http.MethodPatch, g.path + pattern, handler)
+}
+func (g *HttpGroup) Put(pattern string, handler RouteHandler) {
+	g.app.SetHandler(http.MethodPut, g.path + pattern, handler)
 }
