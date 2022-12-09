a9415fbdfa37bcfe42376a9ba3b538200ea2b6af
Лысов Глеб <lysovg9@gmail.com>
2022-12-10 00:18:52 +0300

    Fixed: Do not read body if u doesn't know how to unroll io stream

--- orders/api/loggerMiddleware.go
+++ orders/api/loggerMiddleware.go
@@ -2,18 +2,13 @@ package api
 
 import (
 	"fmt"
-	"io/ioutil"
 	"orders/http"
 	"time"
 )
 
 func LoggerMiddleware(c *http.RouteContext) {
-	body, _ := ioutil.ReadAll(c.Body())
-	bodyString := string(body)
-
 	fmt.Printf("Path:\t%s\n", c.R.URL.Path)
 	fmt.Printf("Method:\t%s\n", c.R.Method)
-	fmt.Printf("Body:\t%s\n", bodyString)
 
 	start := time.Now()
 	c.Next()
